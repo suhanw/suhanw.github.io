ae48973fe01c0383d8ed32d11a544d4418b7cab8
Suhan Wijaya <suhanw@gmail.com>
2021-02-03 14:45:46 -0500

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -42,11 +42,32 @@ const Home = ({ pinnedPosts }) => {
 		};
 	}, []);
 
+	const metaTitle = 'Suhan Wijaya';
+	const metaDescription = 'Right-brained techie who builds apps with JavaScript, React, Redux, Node.js, Hapi, Express, NoSQL, SQL, REST, GraphQL.';
+	const metaImage = 'https://www.suhanwijaya.com/images/digital-garden.png';
+	const metaUrl = 'https://www.suhanwijaya.com/index.html';
+
 	return (
 		<Layout>
 			<Head>
-				<title>Suhan Wijaya</title>
+				{/* Primary Meta Tags */}
+				<title>{metaTitle}</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="title" content={metaTitle} />
+				<meta name="description" content={metaDescription} />
+
+				{/* Open Graph / Facebook */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={metaUrl} />
+				<meta property="og:title" content={metaTitle} />
+				<meta property="og:description" content={metaDescription} />
+				<meta property="og:image" content={metaImage} />
+
+				{/* Twitter */}
+				<meta name="twitter:title" content={metaTitle} />
+				<meta name="twitter:description" content={metaDescription} />
+				<meta name="twitter:image" content={metaImage} />
+				<meta name="twitter:card" content="summary_large_image" />
 			</Head>
 			<Intro />
 			<About />
