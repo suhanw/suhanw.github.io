938dbfde47aabc02668371be19b88554ce86c526
Suhan Wijaya <suhanw@gmail.com>
2021-02-03 19:12:02 -0500

    preload intro image

--- components/intro/index.js
+++ components/intro/index.js
@@ -1,6 +1,10 @@
+import Head from 'next/head';
 import style from './style';
 
-const Intro = (props) => (
+const Intro = () => (<>
+	<Head>
+		<link rel="preload" as="image" href={'/images/digital-garden.png'} />
+	</Head>
 	<section id={'intro-section'} className={style.intro}>
 		<div className={style.imageWrapper}>
 			<span className={style.image} style={{ backgroundImage: `url(/images/digital-garden.png)` }} />
@@ -8,6 +12,6 @@ const Intro = (props) => (
 		<h1 className={style.title}>welcome</h1>
 		<p className={style.subtitle}>to my digital garden.</p>
 	</section>
-);
+</>);
 
 export default Intro;
