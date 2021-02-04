62d9466b5036bf9c62cbbdb7946f6cb9f3034f2b
Suhan Wijaya <suhanw@gmail.com>
2021-02-03 19:12:58 -0500

    publish

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "94020cb0605d3b4d6c5e";
+/******/ 	var hotCurrentHash = "9c5aa070d8d7a6c5fcd4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
