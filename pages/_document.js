a1f75949e0aa86fa06520efb0606ab7a9cfea844
Suhan Wijaya <suhanw@gmail.com>
2021-05-26 16:42:39 -0400

    add Google tag on all pages

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+// https://nextjs.org/docs/advanced-features/custom-document
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ0TSM98BL"></script>
+					<script dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments)}
+							gtag('js', new Date());
+
+							gtag('config', 'G-CZ0TSM98BL');
+					`}} />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
