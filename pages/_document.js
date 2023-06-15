75827e3685fed98f2d4ac59cdaa9d3a3a3ab02ad
Suhan Wijaya <suhanw@gmail.com>
2023-06-14 20:57:06 -0400

    blah

--- pages/_document.js
+++ pages/_document.js
@@ -12,17 +12,18 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-					{process.env.NODE_ENV !== 'development' ?? (<>
-						<script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ0TSM98BL"></script>
-						<script dangerouslySetInnerHTML={{
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ0TSM98BL"></script>
+					<script
+						dangerouslySetInnerHTML={{
 							__html: `
 								window.dataLayer = window.dataLayer || [];
 								function gtag(){dataLayer.push(arguments)}
 								gtag('js', new Date());
 
 								gtag('config', 'G-CZ0TSM98BL');
-						`}} />
-					</>)}
+						`,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
