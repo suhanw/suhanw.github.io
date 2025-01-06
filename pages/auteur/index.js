0b8abc12520b26802092f494b4af3f19dea27747
Suhan Wijaya <suhanw@gmail.com>
2025-01-06 21:42:17 +0800

    Update Auteur live URL

--- pages/auteur/index.js
+++ pages/auteur/index.js
@@ -1,37 +1,39 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const Auteur = () => {
-	const metaTitle = 'auteur by Suhan Wijaya';
-	const metaDescription = 'Auteur is a full-stack blogging app inspired by Tumblr. Express.js backend, MongoDB, and React.js with Redux framework.';
-	const metaImage = 'https://www.suhanwijaya.com/images/auteur.png';
-	const metaUrl = 'https://www.suhanwijaya.com/auteur';
-	
-	return (
-		<Head>
-			{/* Primary Meta Tags */}
-			<title>{metaTitle}</title>
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			<meta name="title" content={metaTitle} />
-			<meta name="description" content={metaDescription} />
+  const metaTitle = "auteur by Suhan Wijaya";
+  const metaDescription =
+    "Auteur is a full-stack blogging app inspired by Tumblr. Express.js backend, MongoDB, and React.js with Redux framework.";
+  const metaImage = "https://www.suhanwijaya.com/images/auteur.png";
+  const metaUrl = "https://www.suhanwijaya.com/auteur";
 
-			{/* Open Graph / Facebook */}
-			<meta property="og:type" content="website" />
-			<meta property="og:url" content={metaUrl} />
-			<meta property="og:title" content={metaTitle} />
-			<meta property="og:description" content={metaDescription} />
-			<meta property="og:image" content={metaImage} />
+  return (
+    <Head>
+      {/* Primary Meta Tags */}
+      <title>{metaTitle}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="title" content={metaTitle} />
+      <meta name="description" content={metaDescription} />
 
-			{/* Twitter */}
-			<meta name="twitter:title" content={metaTitle} />
-			<meta name="twitter:description" content={metaDescription} />
-			<meta name="twitter:image" content={metaImage} />
-			<meta name="twitter:card" content="summary_large_image" />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={metaUrl} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaImage} />
 
-			<script>
-				window.location.replace('https://auteur-nodejs.herokuapp.com');
-			</script>
-		</Head>
-	);
+      {/* Twitter */}
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={metaImage} />
+      <meta name="twitter:card" content="summary_large_image" />
+
+      <script>
+        {/* window.location.replace('https://auteur-nodejs.herokuapp.com'); */}
+        window.location.replace('https://auteur-app.onrender.com/');
+      </script>
+    </Head>
+  );
 };
 
 export default Auteur;
