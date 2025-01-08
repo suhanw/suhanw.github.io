d51a9e90f47664a978b8d67baa523d0b619d87f0
Suhan Wijaya <suhanw@gmail.com>
2025-01-08 10:08:05 +0800

    Fix shifting height in mobile browser UI

--- components/intro/index.js
+++ components/intro/index.js
@@ -1,17 +1,32 @@
-import Head from 'next/head';
-import style from './style';
+import Head from "next/head";
+import style from "./style";
+import { useLayoutEffect, useRef } from "react";
 
-const Intro = () => (<>
-	<Head>
-		<link rel="preload" as="image" href={'/images/digital-garden.png'} />
-	</Head>
-	<section id={'intro-section'} className={style.intro}>
-		<div className={style.imageWrapper}>
-			<span className={style.image} style={{ backgroundImage: `url(/images/digital-garden.png)` }} />
-		</div>
-		<h1 className={style.title}>welcome</h1>
-		<p className={style.subtitle}>to my digital garden.</p>
-	</section>
-</>);
+const Intro = () => {
+  const introRef = useRef();
+  useLayoutEffect(() => {
+    if (introRef.current) {
+      introRef.current.style.height = `${window.innerHeight}px`;
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="preload" as="image" href={"/images/digital-garden.png"} />
+      </Head>
+      <section id={"intro-section"} className={style.intro} ref={introRef}>
+        <div className={style.imageWrapper}>
+          <span
+            className={style.image}
+            style={{ backgroundImage: `url(/images/digital-garden.png)` }}
+          />
+        </div>
+        <h1 className={style.title}>welcome</h1>
+        <p className={style.subtitle}>to my digital garden.</p>
+      </section>
+    </>
+  );
+};
 
 export default Intro;
