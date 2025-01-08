27d039556ccfaf265eaeda0e86e452f18350cc1b
Suhan Wijaya <suhanw@gmail.com>
2025-01-08 21:10:03 +0800

    Update loader

--- components/app-loader/index.js
+++ components/app-loader/index.js
@@ -1,27 +1,55 @@
-import { useEffect } from "react";
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import style from "./style";
 
+const randomGifs = [
+  "/images/puppydogeyes-sad.gif",
+  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMG82eml0YjIwY2V0aDgwbXdldjA5eGpneHM2aG91OWdndmpoOXFzcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lOdm2mFEG0eGmQ/giphy.gif",
+  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGpqemoxYmJ2aXg4bHZieWcwZTR3OWF6YzV1N2RoY3FvOXJuYjNjMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2tLyBxzziMXYdibWHw/giphy.gif",
+  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnNod2UyNDY2MXo5NG9uZDNxNjhmYW9kMGo0dW1waGhmaDQ4cTFraiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/25MomqaozfaDt7Zq8M/giphy.gif",
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function AppLoader({ prefetchUrl, appUrl }) {
+  const [hasError, setHasError] = useState(false);
+
   useEffect(() => {
     fetch(prefetchUrl)
-      .then(() => window.location.replace(appUrl))
-      .catch((err) => console.error(err));
+      .then((res) => {
+        if (!res.ok) {
+          throw res;
+        }
+        window.location.replace(appUrl);
+      })
+      .catch((err) => {
+        console.error(err);
+        setHasError(true);
+      });
   }, []);
 
   return (
     <>
-      <Head>
-        <link rel="preload" as="image" href="/images/loading.jpg" />
-      </Head>
       <article className={style.contentWrapper}>
-        <h2>Warming up my free server...</h2>
-        <p>It's loading, I swear.</p>
+        {!hasError ? (
+          <>
+            <h2>Warming up my free server...</h2>
+            <p>It's loading soon, I swear.</p>
 
-        <figure>
-          <span className={style.loader}></span>
-          <img src="/images/loading.jpg" />
-        </figure>
+            <figure>
+              <img src={randomGifs[getRandomInt(randomGifs.length)]} />
+            </figure>
+          </>
+        ) : (
+          <>
+            <h2>Sorry, I lied. Something went wrong.</h2>
+
+            <figure>
+              <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWJ4bTMxaWR4cXVhaXkxYXBoam1jaXJka2M2cDA1bGVoNnBndm43cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9M5jK4GXmD5o1irGrF/giphy.gif" />
+            </figure>
+          </>
+        )}
       </article>
     </>
   );
