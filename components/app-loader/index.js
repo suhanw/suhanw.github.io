0c2541484323b74b33c9df27ed98848413070de6
Suhan Wijaya <suhanw@gmail.com>
2025-01-08 13:27:03 +0800

    Add app loader to warm up free server instance

---
+++ components/app-loader/index.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import style from "./style";
+
+function AppLoader({ prefetchAssetUrl, appUrl }) {
+  useEffect(() => {
+    fetch(prefetchAssetUrl)
+      .then(() => window.location.replace(appUrl))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <article className={style.contentWrapper}>
+      <h2>Warming up my free server...</h2>
+      <p>I appreciate your patience.</p>
+
+      <figure>
+        <span className={style.loader}></span>
+        <img src="/images/loading.jpg" />
+      </figure>
+    </article>
+  );
+}
+
+export default AppLoader;
--- pages/auteur/index.js
+++ pages/auteur/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import AppLoader from "components/app-loader";
 
 const Auteur = () => {
   const metaTitle = "auteur by Suhan Wijaya";
@@ -8,31 +9,33 @@ const Auteur = () => {
   const metaUrl = "https://www.suhanwijaya.com/auteur";
 
   return (
-    <Head>
-      {/* Primary Meta Tags */}
-      <title>{metaTitle}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="title" content={metaTitle} />
-      <meta name="description" content={metaDescription} />
+    <>
+      <Head>
+        {/* Primary Meta Tags */}
+        <title>{metaTitle}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content={metaTitle} />
+        <meta name="description" content={metaDescription} />
 
-      {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={metaUrl} />
-      <meta property="og:title" content={metaTitle} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:image" content={metaImage} />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={metaUrl} />
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:image" content={metaImage} />
 
-      {/* Twitter */}
-      <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:description" content={metaDescription} />
-      <meta name="twitter:image" content={metaImage} />
-      <meta name="twitter:card" content="summary_large_image" />
+        {/* Twitter */}
+        <meta name="twitter:title" content={metaTitle} />
+        <meta name="twitter:description" content={metaDescription} />
+        <meta name="twitter:image" content={metaImage} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
 
-      <script>
-        {/* window.location.replace('https://auteur-nodejs.herokuapp.com'); */}
-        window.location.replace('https://auteur-app.onrender.com/');
-      </script>
-    </Head>
+      <AppLoader
+        prefetchAssetUrl={"https://auteur-app.onrender.com/scripts/bundle.js"}
+        appUrl={"https://auteur-app.onrender.com/"}
+      />
+    </>
   );
 };
 
