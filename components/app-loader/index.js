5547de0ebb31b7f91fb265c7b8ab47b914afd541
Suhan Wijaya <suhanw@gmail.com>
2025-01-09 08:48:42 +0800

    Update index.js

--- components/app-loader/index.js
+++ components/app-loader/index.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 import style from "./style";
 
-const gifUrls = [
-  "/images/puppydogeyes-sad.gif",
-  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMG82eml0YjIwY2V0aDgwbXdldjA5eGpneHM2aG91OWdndmpoOXFzcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lOdm2mFEG0eGmQ/giphy.gif",
-  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGpqemoxYmJ2aXg4bHZieWcwZTR3OWF6YzV1N2RoY3FvOXJuYjNjMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2tLyBxzziMXYdibWHw/giphy.gif",
-  "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnNod2UyNDY2MXo5NG9uZDNxNjhmYW9kMGo0dW1waGhmaDQ4cTFraiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/25MomqaozfaDt7Zq8M/giphy.gif",
-];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 function AppLoader({ prefetchUrl, appUrl }) {
+  const getRandomGif = () => {
+    const gifs = [
+      "/images/puppydogeyes-sad.gif",
+      "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMG82eml0YjIwY2V0aDgwbXdldjA5eGpneHM2aG91OWdndmpoOXFzcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lOdm2mFEG0eGmQ/giphy.gif",
+      "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGpqemoxYmJ2aXg4bHZieWcwZTR3OWF6YzV1N2RoY3FvOXJuYjNjMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2tLyBxzziMXYdibWHw/giphy.gif",
+      "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMnNod2UyNDY2MXo5NG9uZDNxNjhmYW9kMGo0dW1waGhmaDQ4cTFraiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/25MomqaozfaDt7Zq8M/giphy.gif",
+    ];
+    const randomInt = Math.floor(Math.random() * gifs.length);
+    return gifs[randomInt];
+  };
+
   const [hasError, setHasError] = useState(false);
-  const randomInt = getRandomInt(gifUrls.length);
 
   useEffect(() => {
     fetch(prefetchUrl)
@@ -22,7 +21,7 @@ function AppLoader({ prefetchUrl, appUrl }) {
         if (!res.ok) {
           throw res;
         }
-        window.location.replace(appUrl);
+        // window.location.replace(appUrl);
       })
       .catch((err) => {
         console.error(err);
@@ -39,7 +38,7 @@ function AppLoader({ prefetchUrl, appUrl }) {
             <p>It's loading soon, I swear.</p>
 
             <figure>
-              <img src={gifUrls[randomInt]} />
+              <img src={getRandomGif()} />
             </figure>
           </>
         ) : (
